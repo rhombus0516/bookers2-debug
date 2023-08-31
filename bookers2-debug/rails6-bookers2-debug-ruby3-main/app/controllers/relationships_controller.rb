ab457b6d6dd431b9326f08a70b6ee45531ccd8ea
EC2 Default User <ec2-user@ip-172-31-47-22.ap-northeast-1.compute.internal>
2023-08-31 08:12:31 +0000

    [add]follow,follower数追加

--- bookers2-debug/rails6-bookers2-debug-ruby3-main/app/controllers/relationships_controller.rb
+++ bookers2-debug/rails6-bookers2-debug-ruby3-main/app/controllers/relationships_controller.rb
@@ -1,2 +1,22 @@
 class RelationshipsController < ApplicationController
+    
+    def create
+        currnt_user.follow(params[:user_id])
+        redirect_to users_path(book.user)
+    end
+    
+    def destroy
+        current_user.unfollow(params[:user_id])
+        redirect_to users_path(book.user)
+    end    
+    
+    def followings
+        user = User.find(params[:user_id])
+        @users = user.followings
+    end    
+    
+    def followers
+        user = User.find(params[:user_id])
+        @users = user.followers
+    end    
 end
