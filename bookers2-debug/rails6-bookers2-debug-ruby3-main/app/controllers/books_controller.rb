d988b3ec9dbaec5937e94f3a0fc1d7e88acba506
EC2 Default User <ec2-user@ip-172-31-47-22.ap-northeast-1.compute.internal>
2023-09-06 06:58:10 +0000

    [Add]いいねの合計カウントが多い順に投稿を表示

--- bookers2-debug/rails6-bookers2-debug-ruby3-main/app/controllers/books_controller.rb
+++ bookers2-debug/rails6-bookers2-debug-ruby3-main/app/controllers/books_controller.rb
@@ -12,7 +12,11 @@ class BooksController < ApplicationController
     @user = current_user
     @book = Book.new
     @book_comment = BookComment.new
-    @books = Book.includes(:favorited_users).sort {|a,b| b.favorited_users.size <=> a.favorited_users.size}
+    to = Time.current.at_end_of_day
+    from = (to - 6.day).at_beginning_of_day
+    @books = Book.includes(:favorited_users).sort_by{ |x|
+        x.favorited_users.includes(:favorites).where(created_at: from...to).size
+      }.reverse
   end
 
   def create
