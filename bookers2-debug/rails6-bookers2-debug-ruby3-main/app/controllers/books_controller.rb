e7d28d135ec3370e92f5860beff601fe043ddd3d
EC2 Default User <ec2-user@ip-172-31-47-22.ap-northeast-1.compute.internal>
2023-09-08 02:07:50 +0000

    [Update]閲覧数

--- bookers2-debug/rails6-bookers2-debug-ruby3-main/app/controllers/books_controller.rb
+++ bookers2-debug/rails6-bookers2-debug-ruby3-main/app/controllers/books_controller.rb
@@ -5,6 +5,9 @@ class BooksController < ApplicationController
     @booknew = Book.new
     @user = @book.user
     @book_comment = BookComment.new
+    unless ViewCount.find_by(user_id: current_user.id, book_id: @book.id)
+      current_user.view_counts.create(book_id: @book.id)
+    end 
   end
 
   def index
@@ -16,6 +19,7 @@ class BooksController < ApplicationController
     @books = Book.includes(:favorites).sort_by{ |x|
         x.favorites.where(created_at: from...to).size
       }.reverse
+    
   end
 
   def create
--- bookers2-debug/rails6-bookers2-debug-ruby3-main/app/models/user.rb
+++ bookers2-debug/rails6-bookers2-debug-ruby3-main/app/models/user.rb
@@ -16,7 +16,7 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :followed
   has_many :followers, through: :reverse_of_relationships, source: :follower
   
-  has_many :view_counts, dependent: :dentroy
+  has_many :view_counts, dependent: :destroy
 
   validates :name, length: { minimum: 2, maximum: 20 },presence: true, uniqueness: true
   validates :introduction,length: {maximum: 50}
