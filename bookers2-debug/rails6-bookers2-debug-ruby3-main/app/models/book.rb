35c67471119b0e42deb50add435a31713f391c3e
EC2 Default User <ec2-user@ip-172-31-47-22.ap-northeast-1.compute.internal>
2023-09-08 01:50:17 +0000

    [Add]閲覧数モデル

--- bookers2-debug/rails6-bookers2-debug-ruby3-main/app/models/book.rb
+++ bookers2-debug/rails6-bookers2-debug-ruby3-main/app/models/book.rb
@@ -3,8 +3,8 @@ class Book < ApplicationRecord
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
   has_many :favorites, dependent: :destroy
- 
   has_many :book_comments, dependent: :destroy
+  has_many :view_counts, dependent: :dentroy
 
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
--- bookers2-debug/rails6-bookers2-debug-ruby3-main/app/models/user.rb
+++ bookers2-debug/rails6-bookers2-debug-ruby3-main/app/models/user.rb
@@ -15,6 +15,8 @@ class User < ApplicationRecord
   
   has_many :followings, through: :relationships, source: :followed
   has_many :followers, through: :reverse_of_relationships, source: :follower
+  
+  has_many :view_counts, dependent: :dentroy
 
   validates :name, length: { minimum: 2, maximum: 20 },presence: true, uniqueness: true
   validates :introduction,length: {maximum: 50}
