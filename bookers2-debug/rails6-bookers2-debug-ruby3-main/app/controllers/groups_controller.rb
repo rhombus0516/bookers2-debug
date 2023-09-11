9851208d5258f790758e050b87a45c1eba2c144c
EC2 Default User <ec2-user@ip-172-31-47-22.ap-northeast-1.compute.internal>
2023-09-11 04:48:48 +0000

    [Add]メール

--- bookers2-debug/rails6-bookers2-debug-ruby3-main/app/controllers/groups_controller.rb
+++ bookers2-debug/rails6-bookers2-debug-ruby3-main/app/controllers/groups_controller.rb
@@ -16,9 +16,9 @@ class GroupsController < ApplicationController
   end
   
   def create
-    @grop = Group.new(group_params)
-    @grop.owner_id = current_user.id
-    if @grop.save
+    @group = Group.new(group_params)
+    @group.owner_id = current_user.id
+    if @group.save
       redirect_to groups_path
     else
       render :new
@@ -37,6 +37,18 @@ class GroupsController < ApplicationController
     end
     
   end
+  
+  def new_mail
+    @group = Group.find(params[:group_id])
+  end
+
+  def send_mail
+    @group = Group.find(params[:group_id])
+    group_users = @group.users
+    @mail_title = params[:mail_title]
+    @mail_content = params[:mail_content]
+    ContactMailer.send_mail(@mail_title, @mail_content,group_users).deliver
+end
   
   private
 
