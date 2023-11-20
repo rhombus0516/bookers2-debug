0b98086977d7643a515167436d248116b6151ff3
rhombus0516 <karaya0913@gmail.com>
2023-11-20 01:58:11 +0000

    [Update]アラート表示

--- bookers2-debug/rails6-bookers2-debug-ruby3-main/config/routes.rb
+++ bookers2-debug/rails6-bookers2-debug-ruby3-main/config/routes.rb
@@ -19,14 +19,15 @@ end
   get 'followings' => 'relationships#followings', as: 'followings'
   get 'followers' => 'relationships#followers', as: 'followers'
   get "daily_posts" => "users#daily_posts"
-  
+
   resources :chats, only: [:show, :create]
   end
-  
+
   resources :groups, only: [:new, :index, :show, :create, :edit, :update] do
     resource :group_users, only:[:create, :destroy]
     resources :event_notices, only: [:new, :create]
     get "event_notices" => "event_notices#sent"
-  end  
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end  
+end
